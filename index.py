49c15694c9ea361c4bab2b32b6eca51bc67124d3
Jenna <104884851+jennah1016@users.noreply.github.com>
2025-03-18 14:08:12 -0500

    Add files via upload
    Signed-off-by: Jenna <104884851+jennah1016@users.noreply.github.com>

---
+++ index.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import sys
+
+general = pd.read_csv('https://raw.githubusercontent.com/jennah1016/name-tbd/refs/heads/main/all%20billing%20data.csv')
+electric = pd.read_csv('https://raw.githubusercontent.com/jennah1016/name-tbd/refs/heads/main/Electric%20billing%20data.csv')
+water = pd.read_csv('https://raw.githubusercontent.com/jennah1016/name-tbd/refs/heads/main/Water%20billing%20data.csv')
+natural_gas = pd.read_csv('https://raw.githubusercontent.com/jennah1016/name-tbd/refs/heads/main/Natural%20Gas%20billing%20data.csv')
+
+genheader = ('CTA Utility Spending Since 2024')
+var = len(genheader)
+
+print(genheader)
+for i in range(0, var):
+    sys.stdout.write('-')
+print('\n')
+print(general)
+
+
+labels = general['Category']
+numbers = general['Cost']
+colors = ['yellow', 'red', 'gray', 'mediumpurple', 'deepskyblue']
+explode = (0.2, 0, 0, 0, 0)
+
+plt.pie(numbers, labels=labels, colors=colors, explode=explode)
+plt.legend()
+plt.show()
+
+
