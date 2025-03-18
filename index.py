6ba733a28522b1e6fb9a7c30716354a6044d7635
Jenna <104884851+jennah1016@users.noreply.github.com>
2025-03-18 15:37:32 -0500

    Add files via upload
    Signed-off-by: Jenna <104884851+jennah1016@users.noreply.github.com>

--- index.py
+++ index.py
@@ -8,20 +8,33 @@ water = pd.read_csv('https://raw.githubusercontent.com/jennah1016/name-tbd/refs/
 natural_gas = pd.read_csv('https://raw.githubusercontent.com/jennah1016/name-tbd/refs/heads/main/Natural%20Gas%20billing%20data.csv')
 
 genheader = ('CTA Utility Spending Since 2024')
-var = len(genheader)
+pctheader = ('Utility Billing Percentages Since 2024')
+
+def drawline(str):
+    var = len(str)
+    for i in range(0, var):
+        sys.stdout.write('-')
+    print('\n')
 
 print(genheader)
-for i in range(0, var):
-    sys.stdout.write('-')
-print('\n')
+drawline(genheader)
 print(general)
+
+drawline(genheader)
+
+print(pctheader)
+drawline(pctheader)
+print(general['Category'])
+
+
 
 
 labels = general['Category']
 numbers = general['Cost']
 colors = ['yellow', 'red', 'gray', 'mediumpurple', 'deepskyblue']
-explode = (0.2, 0, 0, 0, 0)
+explode = (0.2, 0, 0, 0.2, 0.2)
 
+plt.title(genheader)
 plt.pie(numbers, labels=labels, colors=colors, explode=explode)
 plt.legend()
 plt.show()
