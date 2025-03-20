e46446aef6a5440f23f11dbf07e9cf0c4115860f
Jenna <104884851+jennah1016@users.noreply.github.com>
2025-03-20 10:54:45 -0500

    Add files via upload
    Signed-off-by: Jenna <104884851+jennah1016@users.noreply.github.com>

--- index.py
+++ index.py
@@ -2,26 +2,29 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import sys
 
+#load data
 general = pd.read_csv('https://raw.githubusercontent.com/jennah1016/name-tbd/refs/heads/main/all%20billing%20data.csv')
 general['Cost'] = round(general['Cost'], 2)
 genpct = general.copy()
 
+#data manipulation
 totalspending = genpct['Cost'].sum()
 
-#data manipulation
 for i in genpct.index:
     genpct.loc[i, 'Cost'] /= totalspending
 
-genpct['Cost'] = round(genpct['Cost'], 4)
+genpct['Cost'] = round(genpct['Cost'], 4)*100
 
+#report headers
 genheader = ('CTA Utility Spending Since 2024')
-pctheader = ('Utility Billing Percentages Since 2024')
+pctheader = ('Utility Spending Percentages Since 2024')
 
 def drawline(str):
     var = len(str)
     for i in range(0, var):
         sys.stdout.write('-')
     print('\n')
+
 
 #general spending report
 print(genheader)
@@ -39,7 +42,7 @@ drawline(pctheader)
 labels = general['Category']
 numbers = general['Cost']
 colors = ['yellow', 'red', 'gray', 'mediumpurple', 'deepskyblue']
-explode = (0.2, 0, 0, 0.2, 0.2)
+explode = (0.2, 0, 0, 0, 0)
 
 plt.title(genheader)
 plt.pie(numbers, labels=labels, colors=colors, explode=explode)
