bc5868c658eaafc9ff8e8a4673fe320ff903ea29
Jenna <104884851+jennah1016@users.noreply.github.com>
2025-03-21 16:26:05 -0500

    Update general reporting.py
    Signed-off-by: Jenna <104884851+jennah1016@users.noreply.github.com>

--- general reporting.py
+++ general reporting.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 electric = pd.read_csv('https://raw.githubusercontent.com/jennah1016/name-tbd/refs/heads/main/Electric%20billing%20data.csv')
 water = pd.read_csv('https://raw.githubusercontent.com/jennah1016/name-tbd/refs/heads/main/Water%20billing%20data.csv')
 natural_gas = pd.read_csv('https://raw.githubusercontent.com/jennah1016/name-tbd/refs/heads/main/Natural%20Gas%20billing%20data.csv')
+spneding_trends = pd.read_csv('https://raw.githubusercontent.com/jennah1016/name-tbd/refs/heads/main/Commodity%20Spending%20Trend.csv')
 
 
 #select data to view
