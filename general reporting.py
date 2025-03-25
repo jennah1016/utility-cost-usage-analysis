8c26f26ae84d5ede2a5be47d4031cd9263a20eb7
Jenna <104884851+jennah1016@users.noreply.github.com>
2025-03-25 14:49:12 -0500

    Add files via upload
    Signed-off-by: Jenna <104884851+jennah1016@users.noreply.github.com>

--- general reporting.py
+++ general reporting.py
@@ -2,6 +2,7 @@ import sys
 import pandas as pd
 import matplotlib.pyplot as plt
 
+#load datasets
 electric = pd.read_csv('https://raw.githubusercontent.com/jennah1016/name-tbd/refs/heads/main/Electric%20billing%20data.csv')
 water = pd.read_csv('https://raw.githubusercontent.com/jennah1016/name-tbd/refs/heads/main/Water%20billing%20data.csv')
 natural_gas = pd.read_csv('https://raw.githubusercontent.com/jennah1016/name-tbd/refs/heads/main/Natural%20Gas%20billing%20data.csv')
@@ -17,7 +18,7 @@ def drawline(str):
         sys.stdout.write('-')
     print('\n')
 
-
+#display options
 print('Select which data you would like to see:')
 def showoptions():
     print('Press (1) for electric billing data')
@@ -31,20 +32,33 @@ user_option = input('Select: ')
 #select data to view
 while True:
     if user_option == '1':
-        print ('electric')
+        chosen_data = electric
+        report_header = "Electric billing data since January 2022"
         break
     elif user_option == '2':
-        print('water')
+        chosen_data = water
+        report_header = "Water billing data since January 2023"
         break
     elif user_option == '3':
-        print('natural gas')
+        chosen_data = natural_gas
+        report_header = "Natural Gas billing data since January 2022"
         break
     elif user_option == '4':
-        print('spending trends')
+        chosen_data = spending_trends
+        report_header = "Utility Spending Trends Over the Past 12 months"
         break
     else:
         showoptions()
         user_option = input('Please choose a valid option:')
+
+#data cleaning
+
 #monthly spending
+print(report_header)
+drawline(report_header)
+print(chosen_data)
+drawline(report_header)
+
 #yearly avg
 #line chart
+plt.title(report_header)
