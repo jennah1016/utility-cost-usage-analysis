0f99d65e78dbb193db657f0d38e2c878e5d0d1fc
Jenna <104884851+jennah1016@users.noreply.github.com>
2025-03-25 14:45:20 -0500

    Update outliers.py
    Signed-off-by: Jenna <104884851+jennah1016@users.noreply.github.com>

--- outliers.py
+++ outliers.py
@@ -2,10 +2,14 @@ import sys
 import pandas as pd
 import matplotlib.pyplot as plt
 
+#load actual data
 general = pd.read_csv('https://raw.githubusercontent.com/jennah1016/name-tbd/refs/heads/main/all%20billing%20data.csv')
 electric = pd.read_csv('https://raw.githubusercontent.com/jennah1016/name-tbd/refs/heads/main/Electric%20billing%20data.csv')
 water = pd.read_csv('https://raw.githubusercontent.com/jennah1016/name-tbd/refs/heads/main/Water%20billing%20data.csv')
 
+#load fake data
+
 #select data to view
+#data manipulation (if necessary)
 #describe function to see what we have to work with
 #histogram
